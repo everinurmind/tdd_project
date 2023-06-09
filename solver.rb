2f37fe8e33bb686b8ca5887c95d617749e04dbb3
everinurmind <nurbol.sultanov@gmail.com>
2023-06-08 18:46:07 -0600

    Fix linters

--- solver.rb
+++ solver.rb
@@ -1,12 +1,12 @@
 class Solver
-    def factorial(num)
-      raise ArgumentError, "Can't calculate factorial for negative numbers" if num.negative?
-      return 1 if num <= 1
-  
-      num * factorial(num - 1)
-    end
-  
-    def reverse(str)
-      str.reverse
-    end
+  def factorial(num)
+    raise ArgumentError, "Can't calculate factorial for negative numbers" if num.negative?
+    return 1 if num <= 1
+
+    num * factorial(num - 1)
   end
+
+  def reverse(str)
+    str.reverse
+  end
+end
