f97ad04b8b37ecf8029a3fc98ea6847796f84e07
everinurmind <nurbol.sultanov@gmail.com>
2023-06-08 18:44:27 -0600

    Add reverse method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,5 +1,4 @@
 require_relative '../solver'
-
 describe Solver do
   describe 'factorial' do
     context 'calculating the factorial' do
@@ -7,7 +6,6 @@ describe Solver do
         result = subject.factorial(4)
         expect(result).to eq 24
       end
-
       it 'returns 1 when input is 0 or 1' do
         result_for_zero = subject.factorial(0)
         result_for_one = subject.factorial(1)
@@ -15,11 +13,21 @@ describe Solver do
         expect(result_for_one).to eq 1
       end
     end
-
     context 'when input is a negative number' do
       it 'should raise error' do
         num = -1
         expect { subject.factorial(num) }.to raise_error(ArgumentError)
+      end
+    end
+  end
+
+  describe 'reverse' do
+    context 'should reverse the word' do
+      it 'returns olleh for word hello' do
+        word = 'hello'
+        expected_result = 'olleh'
+        result = subject.reverse(word)
+        expect(result).to eql expected_result
       end
     end
   end
