aafe385c03503558c1a585e7c871b5be077520ca
everinurmind <nurbol.sultanov@gmail.com>
2023-06-08 18:41:12 -0600

    Add factorial method

---
+++ spec/solver_spec.rb
@@ -0,0 +1,26 @@
+require_relative '../solver'
+
+describe Solver do
+  describe 'factorial' do
+    context 'calculating the factorial' do
+      it 'returns 24 for factorial(4)' do
+        result = subject.factorial(4)
+        expect(result).to eq 24
+      end
+
+      it 'returns 1 when input is 0 or 1' do
+        result_for_zero = subject.factorial(0)
+        result_for_one = subject.factorial(1)
+        expect(result_for_zero).to eq 1
+        expect(result_for_one).to eq 1
+      end
+    end
+
+    context 'when input is a negative number' do
+      it 'should raise error' do
+        num = -1
+        expect { subject.factorial(num) }.to raise_error(ArgumentError)
+      end
+    end
+  end
+end
