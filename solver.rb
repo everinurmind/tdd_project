2dcf7fc80b5cc09e9bf2905e7f36db00fb440b2c
everinurmind <nurbol.sultanov@gmail.com>
2023-06-10 00:21:43 -0600

    Add factorial method

--- solver.rb
+++ solver.rb
@@ -1,2 +1,8 @@
 class Solver
+  def factorial(num)
+    raise ArgumentError, "Can't calculate factorial for negative numbers" if num.negative?
+    return 1 if num <= 1
+
+    num * factorial(num - 1)
+  end
 end
